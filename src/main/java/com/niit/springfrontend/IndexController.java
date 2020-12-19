b3ca4a3f2895915602d81fcd073538ae6c3d41a6
Niit1 <Niit1@DESKTOP-QQU94QK>
2020-12-19 16:58:56 +0530

    abcd

---
+++ src/main/java/com/niit/springfrontend/IndexController.java
@@ -0,0 +1,15 @@
+package com.niit.springfrontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController 
+{
+	@RequestMapping(value= {"","/","/home","/index"})
+	public String m1()
+	{
+		return "index";
+	}
+}
